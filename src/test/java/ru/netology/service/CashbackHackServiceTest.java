3650819a37cd5c454f90aff8c807b011f175970d
Maksis94 <palchenkov94@gmail.com>
2023-10-23 12:04:17 +0300

    junit5

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,29 +1,61 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CashbackHackServiceTest {
-    @org.junit.Test
+    @Test
     public void shouldCalculateCashback0() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assertions.assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldCalculateCashbackBelow1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assertions.assertEquals(service.remain(900), 100);
+    }
+
+    @Test
+    public void shouldCalculateCashbackAbove1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assertions.assertEquals(service.remain(1100), 900);
+    }
+
+    @Test
+    public void shouldCalculateCashback1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assertions.assertEquals(service.remain(1000), 0);
+    }
+
+
+    @org.junit.Test
+    public void shouldCalculateCashback0JUnit4() {
         CashbackHackService service = new CashbackHackService();
 
         org.junit.Assert.assertEquals(service.remain(0), 1000);
     }
 
     @org.junit.Test
-    public void shouldCalculateCashbackBelow1000() {
+    public void shouldCalculateCashbackBelow1000JUnit4() {
         CashbackHackService service = new CashbackHackService();
 
         org.junit.Assert.assertEquals(service.remain(900), 100);
     }
 
     @org.junit.Test
-    public void shouldCalculateCashbackAbove1000() {
+    public void shouldCalculateCashbackAbove1000JUnit4() {
         CashbackHackService service = new CashbackHackService();
 
         org.junit.Assert.assertEquals(service.remain(1100), 900);
     }
 
     @org.junit.Test
-    public void shouldCalculateCashback1000() {
+    public void shouldCalculateCashback1000JUnit4() {
         CashbackHackService service = new CashbackHackService();
 
         org.junit.Assert.assertEquals(service.remain(1000), 0);
